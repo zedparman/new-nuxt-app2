268550a3bf2aff3144e71a5e5c0f445cae7d416d
zedparman <58415963+zedparman@users.noreply.github.com>
2021-09-07 14:52:41 +0430

    Create index.vue

---
+++ client/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <nuxt />
+    <whole />
+  </div>
+</template>
+
+<script>
+import whole from '@/components/whole.vue';
+  export default {
+    comments:{
+      whole
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
